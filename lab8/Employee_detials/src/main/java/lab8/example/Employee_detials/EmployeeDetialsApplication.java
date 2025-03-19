fe14aec6595169f45f5370dcd4311b8526a28f5a
achylas <inshk17@gmail.com>
2025-03-19 12:23:47 +0500

    test

---
+++ lab8/Employee_detials/src/main/java/lab8/example/Employee_detials/EmployeeDetialsApplication.java
@@ -0,0 +1,13 @@
+package lab8.example.Employee_detials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetialsApplication.class, args);
+	}
+
+}
